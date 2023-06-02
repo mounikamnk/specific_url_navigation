957d86ab888015d3a5a6534314c7ba25e091f302
mounikamnk <nasanammounika@gmail.com>
2023-06-02 13:43:47 +0530

    first commit

--- project13/urls.py
+++ project13/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 import csk,rcb
+from india.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sehwal/',sehwal,name='sehwal'),
     path('csk/',include('csk.urls')),
     path('rcb/',include('rcb.urls')),
 ]
